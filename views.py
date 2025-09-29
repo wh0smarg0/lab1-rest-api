88639c6972e4553b4c884fc0861a436a86fcff2a
wh0smarg0 <wh0smarg0.s@gmail.com>
2025-09-29 12:13:44 +0300

    changes

--- views.py
+++ views.py
@@ -2,14 +2,13 @@ from flask import jsonify
 from datetime import datetime
 
 def register_routes(app):
+    @app.route("/", methods=["GET"])
+    def hello():
+        return jsonify({"message": "Hello world!"})
+
     @app.route("/healthcheck", methods=["GET"])
     def healthcheck():
         return jsonify({
             "status": "ok",
             "date": datetime.utcnow().isoformat() + "Z"
         }), 200
-
-    @app.route("/", methods=["GET"])
-    def hello():
-        return jsonify({"message": "Hello world!"})
-
