b3f8e8b146629c2ff8037e9092ab12ee4e82946d
wh0smarg0 <wh0smarg0.s@gmail.com>
2025-09-29 11:23:00 +0300

    feat(routes): реалізовано /healthcheck та / ендпоінти

--- app/views.py
+++ app/views.py
@@ -1,14 +1,15 @@
 from flask import jsonify
 from datetime import datetime
-from app import app
 
-@app.route("/healthcheck", methods=["GET"])
-def healthcheck():
-    return jsonify({
-        "status": "ok",
-        "date": datetime.utcnow().isoformat() + "Z"
-    }), 200
+def register_routes(app):
+    @app.route("/healthcheck", methods=["GET"])
+    def healthcheck():
+        return jsonify({
+            "status": "ok",
+            "date": datetime.utcnow().isoformat() + "Z"
+        }), 200
 
-@app.route("/", methods=["GET"])
-def hello():
-    return jsonify({"message": "Hello, user!"})
+    @app.route("/", methods=["GET"])
+    def hello():
+        return jsonify({"message": "Hello world!"})
+
